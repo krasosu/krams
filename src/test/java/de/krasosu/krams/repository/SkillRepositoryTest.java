1fc3d5b0034cdca685d70710c020b601fc3f19f6
krasosu <krasosu@krasosu.de>
2022-02-23 19:19:38 +0100

    test(repository): injected components unit test

---
+++ src/test/java/de/krasosu/krams/repository/SkillRepositoryTest.java
@@ -0,0 +1,32 @@
+package de.krasosu.krams.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class SkillRepositoryTest {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void injectedComponentsAreNotNull() {
+        assertThat(dataSource).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+        assertThat(entityManager).isNotNull();
+        assertThat(personRepository).isNotNull();
+    }
+
+}
